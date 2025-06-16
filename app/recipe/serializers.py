75c9619251dddbe11e49d57ba7cd83ac0031f4c2
mjavadf <javad.me1376@gmail.com>
2025-06-16 14:29:31 +0200

    Add ingredients handling to RecipeSerializer and corresponding tests

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -26,6 +26,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for the recipe model."""
 
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
@@ -36,6 +37,7 @@ class RecipeSerializer(serializers.ModelSerializer):
             "price",
             "link",
             "tags",
+            "ingredients",
         ]
         read_only_fields = ["id"]
 
@@ -43,14 +45,25 @@ class RecipeSerializer(serializers.ModelSerializer):
         """Handle getting or creating tags for a recipe"""
         auth_user = self.context["request"].user
         for tag in tags:
-            tag_obj, create = Tag.objects.get_or_create(user=auth_user, **tag)
+            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
             recipe.tags.add(tag_obj)
+
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """Handle getting or creating ingredients for a recipe"""
+        auth_user = self.context["request"].user
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
+                user=auth_user, **ingredient
+            )
+            recipe.ingredients.add(ingredient_obj)
 
     def create(self, validated_data):
         """Create a new recipe for the authenticated user."""
         tags = validated_data.pop("tags", [])
+        ingredients = validated_data.pop("ingredients", [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 
@@ -310,3 +310,53 @@ class PrivateRecipeAPITestCase(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.tags.count(), 0)
+
+    def test_create_recipe_with_new_ingredients(self):
+        """Test creating a recipe with new ingredients"""
+        payload = {
+            "title": "New Recipe Title",
+            "time_minutes": 10,
+            "price": Decimal("10.99"),
+            "ingredients": [
+                {"name": "Egg"},
+                {"name": "Cheese"},
+            ],
+        }
+        res = self.client.post(RECIPES_URL, payload, format="json")
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipes = Recipe.objects.filter(user=self.user)
+        self.assertEqual(recipes.count(), 1)
+        recipe = recipes[0]
+        self.assertEqual(recipe.ingredients.count(), 2)
+        for ingredient in payload["ingredients"]:
+            exists = recipe.ingredients.filter(
+                name=ingredient["name"], user=self.user
+            ).exists()
+            self.assertTrue(exists)
+
+    def test_create_recipe_with_existing_ingredients(self):
+        """Test creating a recipe with existing ingredients"""
+        ingredient_egg = Ingredient.objects.create(name="Egg", user=self.user)
+        payload = {
+            "title": "New Recipe Title",
+            "time_minutes": 10,
+            "price": Decimal("10.99"),
+            "ingredients": [
+                {"name": "Egg"},
+                {"name": "Cheese"},
+            ],
+        }
+        res = self.client.post(RECIPES_URL, payload, format="json")
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipes = Recipe.objects.filter(user=self.user)
+        self.assertEqual(recipes.count(), 1)
+        recipe = recipes[0]
+        self.assertEqual(recipe.ingredients.count(), 2)
+        self.assertIn(ingredient_egg, recipe.ingredients.all())
+        for ingredient in payload["ingredients"]:
+            exists = recipe.ingredients.filter(
+                name=ingredient["name"], user=self.user
+            ).exists()
+            self.assertTrue(exists)
