b5d6551799b409563d5a6c01fbfc1dcf537e75db
mjavadf <javad.me1376@gmail.com>
2025-06-18 15:59:04 +0200

    Add image upload functionality to recipe API
    - Introduced RecipeImageSerializer for handling image uploads.
    - Added upload_image action in RecipeViewSet to process image uploads.
    - Updated settings.py with SPECTACULAR_SETTINGS for better schema generation.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,11 +1,14 @@
 """Views for the recipe API."""
 
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from core.models import Recipe, Tag, Ingredient
 from recipe.serializers import (
+    RecipeImageSerializer,
     RecipeSerializer,
     RecipeDetailSerializer,
     TagSerializer,
@@ -29,12 +32,26 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return the serializer class based on the request method."""
         if self.action == "list":
             return RecipeSerializer
+        elif self.action == "upload_image":
+            return RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe for the authenticated user."""
         serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=["post"], url_path="upload-image")
+    def upload_image(self, request, pk=None):
+        """Upload an to recipe."""
+        recipe = self.get_object()
+        serializer = RecipeImageSerializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrViewSet(
